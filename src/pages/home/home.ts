23491384c787b9b945ef0796e98d480e1b3d3596
GustavoMalta <gustavo.a.malta@gmail.com>
2018-09-03 20:02:18 -0300

    First Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+              private media: Media) {
+            
+  }
+  
+         
+
+    public toca(){
+ const arquivo: MediaObject = this.media.create('../assets/lobo.mp3');
+ console.log("Tocando arquivo")
+
+      arquivo.play
+
+
+
+                  
+                }
+
+}
