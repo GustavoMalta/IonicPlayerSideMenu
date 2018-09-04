7be5bb3aa1409ca2f3d298251488b4da50ee8539
GustavoMalta <gustavo.a.malta@gmail.com>
2018-09-03 21:01:40 -0300

    Teste Play

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,7 +17,9 @@ export class HomePage {
          
 
     public toca(){
-    const arquivo: MediaObject = this.media.create('/storage/sdcard0/DCIM/Podcast MdM Melhores do Mundo1499513298.mp3');
+      const arquivo: MediaObject = this.media.create(encodeURI('/storage/sdcard0/DCIM/Podcast MdM Melhores do Mundo1499513298.mp3'));
+
+    //const arquivo: MediaObject = this.media.create('../assets/lobo.mp3');
 
     console.log("Tocando arquivo");
     
